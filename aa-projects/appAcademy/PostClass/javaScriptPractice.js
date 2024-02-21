90ae694fcd2144062f266d175f94772006765a47
Roderick <Roderickj800@gmail.com>
2024-02-21 13:17:00 -0500

    gone

--- aa-projects/appAcademy/PostClass/javaScriptPractice.js
+++ aa-projects/appAcademy/PostClass/javaScriptPractice.js
@@ -348,3 +348,41 @@ function mySome(array, cb) {
   }
   return false;
 }
+
+/*******************************************************************************
+Write a function `count` that accepts an array and a callback as arguments. The
+function should return the number of elements of the array that return true when
+passed to the callback.
+
+Examples:
+
+let result1 = count([18, 5, 32, 7, 100], function (n) {
+    return n % 2 === 0;
+});
+console.log(result1); // 3
+
+let result2 = count([17, 5, 31, 7, 100], function (n) {
+    return n % 2 === 0;
+});
+console.log(result2); // 1
+
+let result3 = count(['follow', 'the', 'yellow', 'brick', 'road'], function (str) {
+    return str.includes('o');
+});
+console.log(result3); // 3
+
+let result4 = count(['follow', 'the', 'yellow', 'brick', 'road'], function (str) {
+    return str.includes('x');
+});
+console.log(result4); // 0
+*******************************************************************************/
+
+function count(array, cb) {
+  let count = 0;
+  for (let ele of array) {
+    if (cb(ele)) {
+      count++;
+    }
+  }
+  return count;
+}
