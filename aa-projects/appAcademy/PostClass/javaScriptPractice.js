fd56bd5fe15efababaf83199207fd62495399e08
Roderick <Roderickj800@gmail.com>
2024-02-24 08:32:22 -0500

    early morning prac

--- aa-projects/appAcademy/PostClass/javaScriptPractice.js
+++ aa-projects/appAcademy/PostClass/javaScriptPractice.js
@@ -668,3 +668,34 @@ function exactly(array, num, cb) {
   }
   return false;
 }
+
+/*******************************************************************************
+Write a function `minValueCallback` that accepts an array and an optional callback as arguments.
+If a callback is not passed in, then the function should return the smallest
+value in the array. If a callback is passed in, then the function should return
+the result of the smallest value being passed into the given callback.
+
+Examples:
+console.log(minValueCallback([64, 25, 49, 9, 100]));             // 9
+console.log(minValueCallback([64, 25, 49, 9, 100], Math.sqrt));  // 3
+
+*******************************************************************************/
+
+function minValueCallback(array, cb) {
+  let value;
+  if (cb === undefined) {
+    for (let ele of array) {
+      if (value > ele || value === undefined) {
+        value = ele;
+      }
+    }
+    return value;
+  } else {
+    for (let ele of array) {
+      if (value > cb(ele) || value === undefined) {
+        value = cb(ele);
+      }
+    }
+  }
+  return value;
+}
