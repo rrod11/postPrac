fc6e054bb7430ec5f9e0747a921710649bed5cc7
Roderick <Roderickj800@gmail.com>
2024-02-27 07:35:18 -0500

    early morning seriousness

--- aa-projects/appAcademy/PostClass/javaScriptPractice.js
+++ aa-projects/appAcademy/PostClass/javaScriptPractice.js
@@ -804,3 +804,49 @@ function suffixCipher(sentence, cipher) {
   });
   return newSentence.join(" ");
 }
+
+/*******************************************************************************
+Write a function `xorSelect` that accepts an array and two callback as arguments.
+The function should return a new array containing elements of the original array
+that result in true when passed in one of the callbacks, but not both.
+
+Examples:
+
+let isEven = function(n) {
+  return n % 2 === 0;
+};
+
+let isPositive = function(n) {
+  return n > 0;
+};
+
+console.log(xorSelect([-2, -1, 1, 2, 3, 4], isEven, isPositive));
+// [ -2, 1, 3 ]
+
+
+let longString = function(s) {
+  return s.length > 4;
+};
+
+let startsA = function(s) {
+  return s[0] === "a";
+};
+
+console.log(
+  xorSelect(["art", "academy", "app", "cat", "buttery"], longString, startsA)
+);
+// [ 'art', 'app', 'buttery' ]
+*******************************************************************************/
+
+let xorSelect = function (array, cb1, cb2) {
+  let arr = [];
+  for (let ele of array) {
+    if (
+      (cb1(ele) === true && cb2(ele) === false) ||
+      (cb1(ele) === false && cb2(ele) === true)
+    ) {
+      arr.push(ele);
+    }
+  }
+  return arr;
+};
