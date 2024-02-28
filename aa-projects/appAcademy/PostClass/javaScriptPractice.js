890cd6bb2c783aa0b3a66a9c66d8c97dd4db6463
Roderick <Roderickj800@gmail.com>
2024-02-28 07:47:20 -0500

    early morning seriousness

--- aa-projects/appAcademy/PostClass/javaScriptPractice.js
+++ aa-projects/appAcademy/PostClass/javaScriptPractice.js
@@ -902,3 +902,33 @@ function one(array, cb) {
   }
   return count === 1;
 }
+
+/*******************************************************************************
+Write a function `greaterCallbackValue` that accepts a value and two callbacks
+as arguments. The function should pass the value to both callbacks and return the
+result of the callback that is greater.
+
+Examples:
+
+let doubler = function (n) {
+    return 2 * n;
+}
+
+let squarer = function (n) {
+    return n * n;
+}
+
+console.log(greaterCallbackValue(5, doubler, squarer));     // 25
+console.log(greaterCallbackValue(1, doubler, squarer));     // 2
+console.log(greaterCallbackValue(9, Math.sqrt, doubler));   // 18
+*******************************************************************************/
+
+function greaterCallbackValue(val, cb1, cb2) {
+  let value;
+  if (cb1(val) > cb2(val)) {
+    value = cb1(val);
+  } else {
+    value = cb2(val);
+  }
+  return value;
+}
