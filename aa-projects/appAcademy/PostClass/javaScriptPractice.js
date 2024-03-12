6d21a419e2f3426e4098c27a3bc1d19172d10fde
Roderick <Roderickj800@gmail.com>
2024-03-12 08:57:23 -0400

    obj manipulation practrice 3

--- aa-projects/appAcademy/PostClass/javaScriptPractice.js
+++ aa-projects/appAcademy/PostClass/javaScriptPractice.js
@@ -1472,3 +1472,22 @@ when printing the object.
 delete obj.first;
 
 console.log(obj); // {}
+
+/*
+Write a function that accepts an object, obj, and a string, str and prints
+the value from the object at the key str.
+*/
+// const obj = {
+//   first: "1",
+//   second: 2,
+//   third: "three",
+// };
+
+const str = "first";
+
+let variableAsKey = (obj, str) => {
+  console.log(obj[str]);
+};
+
+variableAsKey(obj, str); // "1" (looks like 1 in terminal)
+variableAsKey(obj, "second"); // 2
