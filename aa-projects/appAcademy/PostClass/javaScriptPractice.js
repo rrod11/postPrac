ec82b990dac82fe266e32e915f143d26f726fa5b
Roderick <Roderickj800@gmail.com>
2024-03-13 08:42:09 -0400

    obj manipulation practrice

--- aa-projects/appAcademy/PostClass/javaScriptPractice.js
+++ aa-projects/appAcademy/PostClass/javaScriptPractice.js
@@ -1491,3 +1491,26 @@ let variableAsKey = (obj, str) => {
 
 variableAsKey(obj, str); // "1" (looks like 1 in terminal)
 variableAsKey(obj, "second"); // 2
+
+/*
+Write a function catBuilder(name, color, toys) that returns a cat object object with the corresponding properties.
+*/
+// let catBuilder = (name, color, toys) => {
+//   //   let cat = {
+//   //     name: name,
+//   //     color: color,
+//   //     toys: toys,
+//   //   };
+//   //   return cat;
+//   let cat = {};
+//   cat.name = name;
+//   cat.color = color;
+//   cat.toys = toys;
+//   return cat;
+// };
+
+// console.log(catBuilder("Whiskers", "black", ["scratching-post", "yarn"]));
+// // prints: { name: 'Whiskers', color: 'black', toys: ['scratching-post', 'yarn'] }
+
+// console.log(catBuilder("Nyan", "rainbow", ["poptarts"]));
+// // prints: { name: 'Nyan', color: 'rainbow', toys: [ 'poptarts' ] }
