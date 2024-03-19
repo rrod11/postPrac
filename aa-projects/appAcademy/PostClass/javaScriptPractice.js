971760801f840081d24ef65252fc376bdb100f86
Roderick <Roderickj800@gmail.com>
2024-03-19 08:41:38 -0400

    objs

--- aa-projects/appAcademy/PostClass/javaScriptPractice.js
+++ aa-projects/appAcademy/PostClass/javaScriptPractice.js
@@ -1717,3 +1717,28 @@ function appleCounter(appleObj) {
   }
   return count;
 }
+
+/***********************************************************************
+Write a function called `keysInObject(obj)` that takes in an object and returns
+an array of all the keys within that Object.
+
+Do this once using using a `for...in` loop and once using `Object.keys`.
+
+Examples:
+
+let animals = {dog: 'Wolfie', cat: 'Jet', bison: 'Bilbo'}
+let foods = {apple: 'tart', lemon: 'sour', mango: 'sweet'}
+keysInObject(animals); // => ["dog", "cat", "bison"]
+keysInObject(foods); // => ["apple", "lemon", "mango"]
+***********************************************************************/
+
+// function keysInObject(obj) {
+//   return Object.keys(obj);
+// }
+function keysInObject(obj) {
+  let arr = [];
+  for (let key in obj) {
+    arr.push(key);
+  }
+  return arr;
+}
