6aae95ca57a1ef68ff38ad6b737b5cbb8cf9c18f
Roderick <Roderickj800@gmail.com>
2024-03-19 08:42:28 -0400

    objects marathon apparently

--- aa-projects/appAcademy/PostClass/javaScriptPractice.js
+++ aa-projects/appAcademy/PostClass/javaScriptPractice.js
@@ -1789,3 +1789,19 @@ function valuesInObject(obj) {
   }
   return arr;
 }
+
+/***********************************************************************
+Write a function `getFullName(person)` that takes in an person object
+and returns a string containing their full name.
+
+
+Examples:
+let p1 = {firstName: 'John', lastName: 'Doe'};
+getFullName(p1); // => 'John Doe'
+let p2 = {firstName: 'Charlie', lastName: 'Brown', age: 9};
+getFullName(p2); // => 'Charlie Brown'
+***********************************************************************/
+
+function getFullName(person) {
+  return `${person.firstName} ${person.lastName}`;
+}
