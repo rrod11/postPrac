a75f8174a7b640f3e55b6c23b5fff26848b37b80
Roderick <Roderickj800@gmail.com>
2024-03-20 10:25:45 -0400

    late start

--- aa-projects/appAcademy/PostClass/javaScriptPractice.js
+++ aa-projects/appAcademy/PostClass/javaScriptPractice.js
@@ -1847,3 +1847,28 @@ function countScores(people) {
   }
   return object;
 }
+
+/***********************************************************************
+Write a function `stringConverter(string)` that will intake a
+string as an argument and returns an object representing the count of each
+character in the string. **Hint:** don't forget you can check if a key is present
+in an object by using `obj[key] === undefined`.
+
+Examples:
+
+console.log(stringConverter("apple")); // => {a: 1, p: 2, l: 1, e: 1}
+console.log(stringConverter("banana")); // => {b: 1, a: 3, n: 2}
+console.log(stringConverter("raccoon")); // => {r: 1, a: 1, c: 2, o: 2, n: 1}
+***********************************************************************/
+
+function stringConverter(string) {
+  let obj = {};
+  for (let letter of string) {
+    if (obj[letter]) {
+      obj[letter]++;
+    } else {
+      obj[letter] = 1;
+    }
+  }
+  return obj;
+}
