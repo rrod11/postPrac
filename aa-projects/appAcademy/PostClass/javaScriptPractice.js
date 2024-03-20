7a8fd4c3fc8a90d0e97d41e8f6a9c28bff1e70cd
Roderick <Roderickj800@gmail.com>
2024-03-20 10:26:02 -0400

    early finish

--- aa-projects/appAcademy/PostClass/javaScriptPractice.js
+++ aa-projects/appAcademy/PostClass/javaScriptPractice.js
@@ -1872,3 +1872,22 @@ function stringConverter(string) {
   }
   return obj;
 }
+
+/***********************************************************************
+Write a function `valuePair(obj1, obj2, key)` that takes in two objects
+and a key (string). The function should return an array containing the
+corresponding values of the objects for the given key.
+
+Examples:
+let object1 = {name: 'One', location: 'NY', age: 3};
+let object2 = {name: 'Two', location: 'SF'};
+valuePair(object1, object2, 'location'); // => [ 'NY', 'SF' ]
+valuePair(object1, object2, 'name'); // => [ 'One', 'Two' ]
+***********************************************************************/
+
+function valuePair(obj1, obj2, key) {
+  let arr = [];
+  arr.push(obj1[key]);
+  arr.push(obj2[key]);
+  return arr;
+}
