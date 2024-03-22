f78cb4e7f541a9bc06c1629b502a4105566a1381
Roderick <Roderickj800@gmail.com>
2024-03-22 10:12:34 -0400

    does key exist

--- aa-projects/appAcademy/PostClass/javaScriptPractice.js
+++ aa-projects/appAcademy/PostClass/javaScriptPractice.js
@@ -1891,3 +1891,24 @@ function valuePair(obj1, obj2, key) {
   arr.push(obj2[key]);
   return arr;
 }
+
+/***********************************************************************
+Write a function `doesKeyExist(obj, key)` that takes in an object and a
+key and returns true if the key is inside the object and false if the
+key is not inside the object.
+
+Examples:
+
+let obj1 = {bootcamp: 'App Academy', course: 'Bootcamp Prep'}
+doesKeyExist(obj1, 'course'); // => true
+doesKeyExist(obj1, 'name'); // => false
+***********************************************************************/
+
+function doesKeyExist(obj, key) {
+  for (let keys in obj) {
+    if (keys.includes(key)) {
+      return true;
+    }
+  }
+  return false;
+}
