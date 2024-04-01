24d84e5f69ad420a3ee8955cde54cfec8b0142f9
Roderick <Roderickj800@gmail.com>
2024-04-01 09:13:56 -0400

    duplicatessss

--- aa-projects/appAcademy/PostClass/javaScriptPractice.js
+++ aa-projects/appAcademy/PostClass/javaScriptPractice.js
@@ -2081,3 +2081,30 @@ function deleteKeysAndValues(array, keyToDelete) {
   });
   return array;
 }
+
+/***********************************************************************
+Write a function `keyValueDuplicates(obj)` that takes an object as
+an argument and returns an array containing all keys that are also
+values in that object.
+
+Examples:
+
+obj1 = { orange: "juice", apple: "sauce", sauce: "pan" };
+console.log(keyValueDuplicates(obj1)); // ["sauce"]
+
+obj2 = { big: "foot", foot: "ball", ball: "boy", boy: "scout" };
+console.log(keyValueDuplicates(obj2)); // ["foot", "ball", "boy"]
+
+obj3 = { pizza: "pie", apple: "pie", pumpkin: "pie" };
+console.log(keyValueDuplicates(obj3)); // []
+***********************************************************************/
+
+function keyValueDuplicates(obj) {
+  let arr = [];
+  for (let keys in obj) {
+    if (Object.values(obj).includes(keys)) {
+      arr.push(keys);
+    }
+  }
+  return arr;
+}
