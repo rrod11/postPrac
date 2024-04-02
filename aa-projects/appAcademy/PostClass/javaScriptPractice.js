f5a11a34916b41def21331a3913040fb4bdc837e
Roderick <Roderickj800@gmail.com>
2024-04-02 10:10:48 -0400

    we taxing over here

--- aa-projects/appAcademy/PostClass/javaScriptPractice.js
+++ aa-projects/appAcademy/PostClass/javaScriptPractice.js
@@ -2172,3 +2172,26 @@ function employeeRaises(employees) {
   }
   return arr;
 }
+
+/***********************************************************************
+Write a function using fat arrow syntax, `salesTax` that takes in an arbitrary
+number of arguments as prices, adds them up and returns a string containing the
+sales tax with a dollar sign in front. Use a tax percentage of 9%.
+
+Hint: Look up `toFixed()` on MDN
+
+Examples:
+
+console.log(salesTax(30, 70)); // '$9.00'
+console.log(salesTax(1.12, 2.23, 3.34, 4.45)); // '$1.00'
+console.log(salesTax(4.99, 9.99, 19.99)); // '$3.15'
+***********************************************************************/
+
+const salesTax = (...prices) => {
+  let sum = 0;
+  for (let price of prices) {
+    sum += price;
+  }
+  sum *= 0.09;
+  return `$${sum.toFixed(2)}`;
+};
