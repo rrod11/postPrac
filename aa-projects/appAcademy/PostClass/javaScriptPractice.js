a253ba07f5f921e813b5aa941e0b19ce9cd840a1
Roderick <Roderickj800@gmail.com>
2024-04-10 09:24:49 -0400

    wed

--- aa-projects/appAcademy/PostClass/javaScriptPractice.js
+++ aa-projects/appAcademy/PostClass/javaScriptPractice.js
@@ -2580,3 +2580,41 @@ Given an object, obj, declare a method, printValues on obj that prints all uniqu
 // };
 
 // printUniqueValues(obj); // Honda Kia Mercedes BMW
+
+/*
+Given the function below that iterates through an object and prints all values
+associated with keys that are vowels using Object.keys(), refactor the code
+to use the for...in pattern to iterate through the object.
+
+Key point here is to note how there are multiple ways to iterate through an
+object that are interchangeable. Comment out the original function to run your refactored function.
+*/
+
+// const obj = {
+//   a: "get",
+//   b: "all",
+//   c: "of",
+//   d: "the",
+//   e: "keys",
+// };
+
+// // const printValues = (obj) => {
+// //   const vowels = ["a", "e", "i", "o", "u"];
+// //   for (let key of Object.keys(obj)) {
+// //     if (vowels.indexOf(key) >= 0) {
+// //       console.log(obj[key]);
+// //     }
+// //   }
+// // };
+
+// const printValuesRefactored = (obj) => {
+//   const vowels = ["a", "e", "i", "o", "u"];
+//   for (let key in obj) {
+//     if (vowels.indexOf(key) >= 0) {
+//       console.log(obj[key]);
+//     }
+//   }
+// };
+
+// // printValues(obj); // get values
+// printValuesRefactored(obj); // get values
