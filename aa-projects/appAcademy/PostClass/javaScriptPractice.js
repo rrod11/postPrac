d3bdfb7aee40d99770657b4bd9977858f301daca
Roderick <Roderickj800@gmail.com>
2024-04-10 09:25:20 -0400

    object includes

--- aa-projects/appAcademy/PostClass/javaScriptPractice.js
+++ aa-projects/appAcademy/PostClass/javaScriptPractice.js
@@ -2618,3 +2618,24 @@ object that are interchangeable. Comment out the original function to run your r
 
 // // printValues(obj); // get values
 // printValuesRefactored(obj); // get values
+
+/*
+Write a function includedInObject that accepts an object, obj, and a key
+and returns true if that key exists on the object or false otherwise.
+*/
+// const obj = {
+//   first: "1",
+//   second: 2,
+//   third: "three",
+// };
+
+// const includedInObject = (obj, key) => {
+//   if (obj[key]) {
+//     return true;
+//   }
+//   return false;
+// };
+
+// console.log(includedInObject(obj, "first")); // true
+// console.log(includedInObject(obj, "second")); // true
+// console.log(includedInObject(obj, "fourth")); // false
