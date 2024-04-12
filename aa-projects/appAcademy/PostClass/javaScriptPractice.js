f938fb705d57681d43e2e886335f8b8b6198a7ca
Roderick <Roderickj800@gmail.com>
2024-04-12 12:34:39 -0400

    m,ultiple chara

--- aa-projects/appAcademy/PostClass/javaScriptPractice.js
+++ aa-projects/appAcademy/PostClass/javaScriptPractice.js
@@ -2664,3 +2664,34 @@ The function should return true if that value is indeed a value in the object, o
 // console.log(valInObject(obj, "pot")); // true
 // console.log(valInObject(obj, "cup")); // false
 // console.log(valInObject(obj, "fork")); // false
+
+/*
+Given a sentence string, sentence, write a function that returns the most
+common character within the sentence. If there are multiple characters that
+appear the most, return the lexicographically smallest one (e.g. if 'a' and 'b' are both the most common, return 'a' because it is closest to the beginning of the alphabet). Be sure to exclude spaces when counting
+characters.
+// */
+// const sentence = "What is the most common character in this sentence";
+
+// const counter = (sentence) => {
+//   let obj = {};
+//   for (let letter of sentence) {
+//     if (letter === " ") continue;
+//     if (obj[letter]) {
+//       obj[letter]++;
+//     } else {
+//       obj[letter] = 1;
+//     }
+//   }
+//   let value = 0;
+//   let goal;
+//   for (let key in obj) {
+//     if (obj[key] > value) {
+//       value = obj[key];
+//       goal = key;
+//     }
+//   }
+//   return goal;
+// };
+
+// console.log(counter(sentence)); //  "t"
