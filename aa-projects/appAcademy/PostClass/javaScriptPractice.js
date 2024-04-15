7498cd5df913bc4d1f91af089e82eaf6c22e7ef3
Roderick <Roderickj800@gmail.com>
2024-04-15 16:31:58 -0400

    sorting by hand

--- aa-projects/appAcademy/PostClass/javaScriptPractice.js
+++ aa-projects/appAcademy/PostClass/javaScriptPractice.js
@@ -2783,3 +2783,16 @@ Write a function named restSum that accepts all incoming parameters and sums the
 
 // const result2 = myReduce(testArr, add, 5)
 // console.log(result2) // 15
+
+/*
+Write a function named spreadItOut(array1, array2) that accepts two arrays and
+uses spread operator syntax to return a single array.
+*/
+
+// const spreadItOut = (array1, array2) => {
+//   return (newarr = [...array1, ...array2]);
+// };
+
+// console.log(spreadItOut([3, 5, 6], [1, 2, 3])); // => [3,5,6,1,2,3];
+// console.log(spreadItOut([], [1, 2, 3])); // => [1,2,3];
+// console.log(spreadItOut(["apple", "banana"], [1, 2, 3])); // => ["apple", "banana", 1, 2, 3];
