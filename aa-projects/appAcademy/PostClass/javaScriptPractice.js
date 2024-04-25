b8bcfc6c7d25373be20021cd51485137b500a0d5
Roderick <Roderickj800@gmail.com>
2024-04-25 17:50:44 -0400

    objIteration

--- aa-projects/appAcademy/PostClass/javaScriptPractice.js
+++ aa-projects/appAcademy/PostClass/javaScriptPractice.js
@@ -2904,3 +2904,32 @@ characters.
 // };
 
 // console.log(objectCount(blackClover));
+
+/*
+Given the array friends below, use the Array.forEach() method to iterate through each element and print the name and yearsOfFriendship properties stylized as "name: yearsOfFriendship years".
+
+For example: "Albert: 3 years"
+*/
+
+// const friends = [
+//   {
+//     name: "Albert",
+//     yearsOfFriendship: 3,
+//   },
+//   {
+//     name: "Angela",
+//     yearsOfFriendship: 2,
+//   },
+//   {
+//     name: "Freddy",
+//     yearsOfFriendship: 8,
+//   },
+//   {
+//     name: "Agatha",
+//     yearsOfFriendship: 6,
+//   },
+// ];
+
+// friends.forEach((item) => {
+//   console.log(`${item.name}: ${item.yearsOfFriendship} years`);
+// });
