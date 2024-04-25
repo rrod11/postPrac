4999ac36ef5ef6a656ba61ba5fbeb3213adada62
Roderick <Roderickj800@gmail.com>
2024-04-25 17:49:51 -0400

    objClone

--- aa-projects/appAcademy/PostClass/javaScriptPractice.js
+++ aa-projects/appAcademy/PostClass/javaScriptPractice.js
@@ -2836,3 +2836,23 @@ into arrRest/arrSpread and objRest/objSpread respectively.
 
 // console.log(objRest); // { red: "circle", blue: "square", green: "hexagon" }
 // console.log(objSpread); // { red: "circle", blue: "square", green: "hexagon" }
+
+/*
+Given obj below, use a combination of the spread and rest operator in a single
+statement to create a clone objClone that removes the green key-value pair
+and adds a yellow key with value pentagon.
+
+Hint: As a side effect, you will end up creating a green variable with value
+"hexagon".
+*/
+
+// const obj = {
+//   red: "circle",
+//   blue: "square",
+//   green: "hexagon",
+// };
+
+// let { green, ...objClone } = { ...obj, yellow: "pentagon" };
+
+// console.log(objClone); // { red: "circle", blue: "square", yellow: "pentagon" }
+// console.log(green); // hexagon
