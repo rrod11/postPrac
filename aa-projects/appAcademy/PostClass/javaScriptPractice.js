5e7787d20cdd06c36abe7a240b7d8a8a9d629464
Roderick <Roderickj800@gmail.com>
2024-05-05 04:55:53 -0400

    sumton

--- aa-projects/appAcademy/PostClass/javaScriptPractice.js
+++ aa-projects/appAcademy/PostClass/javaScriptPractice.js
@@ -3521,3 +3521,24 @@ const calendar5 = [
 ];
 
 console.log(adequateWaterTracker(calendar5)); // true
+
+/***********************************************************************
+Write a recursive function called `sumToN` that takes in a number and returns
+the sum of all the numbers from 0 to that number. Return null for any input
+number below 0.
+
+Examples:
+
+sumToN(5) // returns 15
+sumToN(1)  // returns 1
+sumToN(9)  // returns 45
+sumToN(-8)  // returns null
+***********************************************************************/
+
+function sumToN(n) {
+  let sum = 0;
+  if (n < 0) return null;
+
+  sum += n;
+  return sumToN(n - 1) + sum;
+}
