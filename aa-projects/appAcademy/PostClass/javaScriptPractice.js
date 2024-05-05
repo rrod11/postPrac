4007fffe756a1b9e542373d73eae5164a0995b92
Roderick <Roderickj800@gmail.com>
2024-05-05 04:56:14 -0400

    sumarrray

--- aa-projects/appAcademy/PostClass/javaScriptPractice.js
+++ aa-projects/appAcademy/PostClass/javaScriptPractice.js
@@ -3542,3 +3542,23 @@ function sumToN(n) {
   sum += n;
   return sumToN(n - 1) + sum;
 }
+
+/***********************************************************************
+Write a recursive function called `sumArray` that takes an array of integers
+and returns the value of all the integers added together. Your array may
+include a mix of positive and negative integers!
+
+Examples:
+
+sumArray([1, 2, 3]); //  6
+sumArray([0, 1, -3]); //  -2
+sumArray([1, 2, 3, 4, 5]); // 15
+***********************************************************************/
+
+function sumArray(arr) {
+  if (arr.length === 0) return 0;
+
+  let sum = arr[0];
+
+  return sumArray(arr.slice(1)) + sum;
+}
