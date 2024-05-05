111f081b773d058f848a2bb40f725423215f8484
Roderick <Roderickj800@gmail.com>
2024-05-05 04:56:52 -0400

    range

--- aa-projects/appAcademy/PostClass/javaScriptPractice.js
+++ aa-projects/appAcademy/PostClass/javaScriptPractice.js
@@ -3584,3 +3584,26 @@ function iceCreamShop(flavors, favorite) {
 
   return iceCreamShop(flavors.slice(1), favorite);
 }
+
+/***********************************************************************
+Write a recursive function, `range`, that takes a start and an end and returns
+an array of all numbers in that range, exclusive. If the end number is less than
+the start, return an empty array.
+
+Examples:
+
+range(1, 5); // [1, 2, 3, 4]
+range(3, 4); // [3]
+range(7, 6); // []
+***********************************************************************/
+
+function range(start, end) {
+  if (end <= start) {
+    return [];
+  }
+
+  return [start].concat(range(start + 1, end));
+}
+console.log(range(1, 5)); // [1, 2, 3, 4]
+console.log(range(3, 4)); // [3]
+console.log(range(7, 6)); // []
