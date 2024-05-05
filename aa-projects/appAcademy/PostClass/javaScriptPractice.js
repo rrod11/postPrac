ef916522d2ad7af68d04c326c392ad92a1ce6b14
Roderick <Roderickj800@gmail.com>
2024-05-05 04:58:04 -0400

    12

--- aa-projects/appAcademy/PostClass/javaScriptPractice.js
+++ aa-projects/appAcademy/PostClass/javaScriptPractice.js
@@ -3641,3 +3641,25 @@ function reverse(str) {
   let fLetter = str[0];
   return reverse(str.slice(1)) + fLetter;
 }
+
+/***********************************************************************
+Write a recursive function called `addToTwelve` that will return true if there
+are two adjacent numbers in the input array that can sum up to 12. Otherwise,
+return false.
+
+Examples:
+
+addToTwelve([1, 3, 4, 7, 5]); // true
+addToTwelve([1, 3, 4, 7, 6]); // false
+addToTwelve([1, 11, 4, 7, 6]); // true
+addToTwelve([1, 12, 4, 7, 6]); // false
+addToTwelve([1]); // false
+***********************************************************************/
+
+function addToTwelve(arr) {
+  if (arr.length < 2) return false;
+
+  if (arr[0] + arr[1] === 12) return true;
+
+  return addToTwelve(arr.splice(1));
+}
