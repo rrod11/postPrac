ce54afe08f1f47e29f85a3a96eb0b2a8cb9ee7e0
Roderick <Roderickj800@gmail.com>
2024-05-05 04:56:34 -0400

    icercreamshop

--- aa-projects/appAcademy/PostClass/javaScriptPractice.js
+++ aa-projects/appAcademy/PostClass/javaScriptPractice.js
@@ -3562,3 +3562,25 @@ function sumArray(arr) {
 
   return sumArray(arr.slice(1)) + sum;
 }
+
+/***********************************************************************
+Write a recursive function `iceCreamShop(flavors, favorite)` that takes in an
+array of ice cream flavors available at the ice cream shop, as well as the
+user's favorite ice cream flavor. Recursively find out whether or not the shop
+offers their favorite flavor.
+
+Examples:
+iceCreamShop(['vanilla', 'strawberry'], 'blue moon'); // false
+iceCreamShop(['pistachio', 'green tea', 'chocolate', 'mint chip'], 'green tea'); // true
+iceCreamShop(['cookies n cream', 'blue moon', 'superman', 'honey lavender', 'sea salt caramel'], 'pistachio'); // false
+iceCreamShop(['moose tracks'], 'moose tracks'); // true
+iceCreamShop([], 'honey lavender'); // false
+***********************************************************************/
+
+function iceCreamShop(flavors, favorite) {
+  if (flavors.length === 0) return false;
+
+  if (flavors[0] === favorite) return true;
+
+  return iceCreamShop(flavors.slice(1), favorite);
+}
