98b97bfed3e106b617df8f1bc3dc505befcc0396
Roderick <Roderickj800@gmail.com>
2024-05-12 19:57:57 -0400

    isSorted

--- aa-projects/appAcademy/PostClass/javaScriptPractice.js
+++ aa-projects/appAcademy/PostClass/javaScriptPractice.js
@@ -3836,3 +3836,15 @@ function flatten(arr) {
 console.log(flatten([])); // []
 console.log(flatten([1, 2])); // [1, 2]
 console.log(flatten([1, [2, [3]]])); // [1, 2, 3]
+
+/***********************************************************************
+Write a recursive solution called `isSorted` to determine if the input array
+is sorted in ascending order.
+
+Examples:
+
+isSorted([1, 2, 3, 4, 5]); // true
+isSorted([1, 2, 4, 3, 5]); // false
+isSorted([2, 4, 6, 7, 8]); // true
+isSorted([5, 4, 3, 2, 1]); // false
+***********************************************************************/
