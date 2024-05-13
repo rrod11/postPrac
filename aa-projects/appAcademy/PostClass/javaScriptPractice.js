ed54fdfb45fb4f97156ff3e2c0873ad602b02629
Roderick <Roderickj800@gmail.com>
2024-05-12 20:00:52 -0400

    subsets

--- aa-projects/appAcademy/PostClass/javaScriptPractice.js
+++ aa-projects/appAcademy/PostClass/javaScriptPractice.js
@@ -3940,3 +3940,21 @@ function deepDup(arr) {
   }
   return array;
 }
+
+/***********************************************************************
+ * BONUS PROBLEM
+************************************************************************
+Write a function called `subsets` that will return all subsets of an array.
+
+Examples:
+
+subsets([]) // [[]]
+subsets([1]) // [[], [1]]
+subsets([1, 2]) // [[], [1], [2], [1, 2]]
+subsets([1, 2, 3]) // [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]
+
+Hint: For subsets([1, 2, 3]), there are two kinds of subsets:
+  1. Those that do not contain 3 (all of these are subsets of [1, 2]).
+  2. For every subset that does not contain 3, there is also a corresponding
+     subset that is the same, except it also does contain 3.
+***********************************************************************/
