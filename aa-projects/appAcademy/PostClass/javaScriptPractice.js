c801ca2e86b6f3a4bd4488773b38b687beb6575f
Roderick <Roderickj800@gmail.com>
2024-05-19 10:07:28 -0400

    r1

--- aa-projects/appAcademy/PostClass/javaScriptPractice.js
+++ aa-projects/appAcademy/PostClass/javaScriptPractice.js
@@ -4372,3 +4372,20 @@ const rl = readline.createInterface({
 
 // // try to print 'DONE!' after the question
 // console.log("DONE!");
+
+rl.question("What's up, doc? ", (answer) => {
+  console.log("you responded: " + answer);
+  rl.close();
+  console.log("DONE!");
+});
+
+// ask question one
+rl.question("What's up, doc? ", (firstAnswer) => {
+  console.log(firstAnswer + " is up.");
+
+  // only after the user responds to question one, then ask question two
+  rl.question("What's down, clown? ", (secondAnswer) => {
+    console.log(secondAnswer + " is down.");
+    rl.close();
+  });
+});
