af425d331610258d2d0bd3e0985f65e929cdeb06
Roderick <Roderickj800@gmail.com>
2024-05-19 10:04:45 -0400

    permutations

--- aa-projects/appAcademy/PostClass/javaScriptPractice.js
+++ aa-projects/appAcademy/PostClass/javaScriptPractice.js
@@ -4055,3 +4055,43 @@ function makeTree(categories, parent = null) {
   });
   return tree;
 }
+
+/***********************************************************************
+ * BONUS PROBLEM
+************************************************************************
+Write a recursive method permutations(array) that calculates all the
+permutations of the given array. For an array of length n there are n! different
+permutations. So for an array with three elements we will have 3 * 2 * 1 = 6
+different permutations.
+
+Examples:
+
+permutations([1, 2]) // [[1, 2], [2, 1]]
+permutations([1, 2, 3]) // [[1, 2, 3], [1, 3, 2],
+                        // [2, 1, 3], [2, 3, 1],
+                        // [3, 1, 2], [3, 2, 1]]
+***********************************************************************/
+
+function permutations(array) {
+  if (array.length === 1) {
+    return [array];
+  }
+
+  const allPermutations = [];
+
+  for (let i = 0; i < array.length; i++) {
+    const currentElement = array[i];
+    const remainingElements = array.slice(0, i).concat(array.slice(i + 1));
+    const permutationsOfRemaining = permutations(remainingElements);
+
+    for (const permutation of permutationsOfRemaining) {
+      allPermutations.push([currentElement, ...permutation]);
+    }
+  }
+
+  return allPermutations;
+}
+
+// Test cases
+console.log(permutations([1, 2])); // Output: [[1, 2], [2, 1]]
+console.log(permutations([1, 2, 3])); // Output: [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
