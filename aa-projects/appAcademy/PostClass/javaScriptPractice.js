a87a3e62464b4abfcfed6dcdde5e23d4c9aa3ddf
Roderick <Roderickj800@gmail.com>
2024-06-02 07:47:24 -0400

    rotate animzls

--- aa-projects/appAcademy/PostClass/javaScriptPractice.js
+++ aa-projects/appAcademy/PostClass/javaScriptPractice.js
@@ -4667,3 +4667,7 @@ function rotate(arr, num) {
 let arr = ["a", "b", "c", "d", "e"];
 rotate(arr, 2);
 console.log(arr); // [ 'd', 'e', 'a', 'b', 'c' ]
+
+let animals = ["wombat", "koala", "opossum", "kangaroo"];
+rotate(animals, -1);
+console.log(animals); // [ 'koala', 'opossum', 'kangaroo', 'wombat' ]
