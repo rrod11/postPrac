e670a4cc2a41b5b1560fd5bfb2be30d5e865d7d3
Roderick <Roderickj800@gmail.com>
2024-07-29 07:56:15 -0400

    start

--- aa-projects/appAcademy/PostClass/javaScriptPractice.js
+++ aa-projects/appAcademy/PostClass/javaScriptPractice.js
@@ -5237,3 +5237,15 @@ function printOuterNumsFirst(nums) {
   printOuterNumsFirst(nums); //recursively call the modified nums array until empty
 }
 /*
+
+printOuterNumsFirst([1, 2, 3, 4, 5, 6, 7, 8]);
+// 1
+// 8
+// 2
+// 7
+// 3
+// 6
+// 4
+// 5
+
+*/
