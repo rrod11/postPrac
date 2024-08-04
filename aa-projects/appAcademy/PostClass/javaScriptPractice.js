f17f88afe9c023dac8ff3a15e4028c7d94b184ed
Roderick <Roderickj800@gmail.com>
2024-08-04 09:24:44 -0400

    constnat

--- aa-projects/appAcademy/PostClass/javaScriptPractice.js
+++ aa-projects/appAcademy/PostClass/javaScriptPractice.js
@@ -5324,3 +5324,5 @@ function squared2(input) {
 /*
 If the given input is an array of numbers, return the sum of all the positives ones. If the array is empty or there aren't any positive numbers, return 0.
 */
+
+// const input = [1, -4, 12, 0, -3, 29, -150];
