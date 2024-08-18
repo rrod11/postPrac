0cb81c4efa47ee1a1de6969f7d618eaf3c63dfbc
Roderick <Roderickj800@gmail.com>
2024-08-18 09:03:50 -0400

    pa2

--- aa-projects/appAcademy/PostClass/javaScriptPractice.js
+++ aa-projects/appAcademy/PostClass/javaScriptPractice.js
@@ -5408,3 +5408,7 @@ function ageDifference(input) {
 }
 
 // console.log(ageDifference(input));
+
+/*
+Devs like to abbreviate everything: k8s means Kubernetes, a11y means accessibility, l10n means localization. You get the Dev numeronyms by taking the first and the last letter and counting the number of letters in between. Words that have less than 4 letters aren't abbreviated, because that would just be odd. The input is a sentence, and you should abbreviate every word that is 4 letters long or longer. There won't be any punctuation in the sentence. g2d l2k e6e
+*/
