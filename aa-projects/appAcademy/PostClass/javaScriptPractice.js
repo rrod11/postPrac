46574e3e193736f34d9370375e4b5ae4b4d62bfe
Roderick <Roderickj800@gmail.com>
2024-08-18 09:02:46 -0400

    pa const

--- aa-projects/appAcademy/PostClass/javaScriptPractice.js
+++ aa-projects/appAcademy/PostClass/javaScriptPractice.js
@@ -5372,3 +5372,26 @@ function initial(name) {
 /*
 Find the difference in age between the oldest and youngest family members, and return their respective ages and the age difference.
 */
+
+// const input = [
+//   {
+//     name: "John",
+//     age: 13,
+//   },
+//   {
+//     name: "Mark",
+//     age: 56,
+//   },
+//   {
+//     name: "Rachel",
+//     age: 45,
+//   },
+//   {
+//     name: "Nate",
+//     age: 67,
+//   },
+//   {
+//     name: "Jennifer",
+//     age: 65,
+//   },
+// ];
