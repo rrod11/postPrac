f836a2b7d59740883db35a618c644ebc9c84ba57
Roderick <Roderickj800@gmail.com>
2024-08-25 18:39:45 -0400

    cd

--- aa-projects/appAcademy/PostClass/javaScriptPractice.js
+++ aa-projects/appAcademy/PostClass/javaScriptPractice.js
@@ -5433,3 +5433,8 @@ function numeronyms(str) {
 /*
 If the given input is a number, you should return the factorial of that number. The factorial of a natural number n is the product of the positive integers less than or equal to n. So, 2! = 2, 3! = 6, 4! = 24 and so on.
 */
+// function countDown(num) {
+//   for (let i = num; i < 0; i--) {
+//     return i;
+//   }
+// }
