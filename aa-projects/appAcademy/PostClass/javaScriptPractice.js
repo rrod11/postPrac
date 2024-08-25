5fd3d47a3893609d2b5fb2a3643baa093c680786
Roderick <Roderickj800@gmail.com>
2024-08-25 18:41:54 -0400

    ?2 expectations

--- aa-projects/appAcademy/PostClass/javaScriptPractice.js
+++ aa-projects/appAcademy/PostClass/javaScriptPractice.js
@@ -5458,3 +5458,12 @@ Count the occurrences of distinct elements in the given 2D array. The given inpu
 //   ["c", "d", "f"],
 //   ["d", "f", "g"],
 // ];
+
+//{
+//    a: 1,
+//    b: 1,
+//    c: 2,
+//    d: 2,
+//    f: 2,
+//    g: 1,
+//  }
