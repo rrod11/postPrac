95e0ba67f0ba25b240819e817f71a6026322c3b3
Roderick <Roderickj800@gmail.com>
2024-09-30 07:22:51 -0400

    9

--- aa-projects/appAcademy/PostClass/javaScriptPractice.js
+++ aa-projects/appAcademy/PostClass/javaScriptPractice.js
@@ -5690,3 +5690,14 @@ function r2d2Speaks(code) {
 
 let code = [0, 1, 1, 0];
 r2d2Speaks(code);
+
+/*
+    Recursive Sort
+
+    Given an array of numbers, write a function that returns a new array of numbers sorted from lowest to highest.
+
+    console.log(sort([])) // prints []
+    console.log(sort([9])) // prints [9]
+    console.log(sort([5, 4, 3, 2, 1])) // prints [1, 2, 3, 4, 5]
+    console.log(sort([14, 5, 10, 6, 3, 4, 21, 16, 9])); // prints [ 3, 4, 5, 6, 9, 10, 14, 16, 21 ]
+*/
