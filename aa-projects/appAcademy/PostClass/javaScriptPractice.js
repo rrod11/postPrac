e9d52373957fa5a6e5719b7343ceea4ee2b29089
Roderick <Roderickj800@gmail.com>
2024-10-06 09:02:08 -0400

    6

--- aa-projects/appAcademy/PostClass/javaScriptPractice.js
+++ aa-projects/appAcademy/PostClass/javaScriptPractice.js
@@ -5829,3 +5829,9 @@ console.log(preserveFunc2('object')); // prints [ { color: 'red' }, [4, 5] ];
 console.log(preserveFunc2('boolean', 'string', 'undefined')); // prints [ undefined, 'world', true, 'hello', false ]
 
 */
+
+function preserveTypes(array) {
+  return function (...types) {
+    return array.filter((ele) => types.includes(typeof ele));
+  };
+}
