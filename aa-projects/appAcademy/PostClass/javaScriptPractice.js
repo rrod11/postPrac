4397ef2e4b53cefe52d9b2f782f1ac88492af117
Roderick <Roderickj800@gmail.com>
2024-11-11 08:20:36 -0500

    10

--- aa-projects/appAcademy/PostClass/javaScriptPractice.js
+++ aa-projects/appAcademy/PostClass/javaScriptPractice.js
@@ -6158,3 +6158,14 @@ function recursivePreserveType(array) {
 
 // Following the guidelines of SRP sets up easier to read code that just makes more
 // sense.
+
+// ## DRY - Don't Repeat Yourself
+
+// Making code that is more readable and more reusable should be every programmer's
+// goal. By taking a critical look at your code and identifying places where you
+// may have identical logic repeated throughout and creating smaller, more reusable
+// functions or methods to contain that logic, allows you to write code that other
+// programmers will be able to follow more closely and accurately, leading to
+// maintainable code.
+
+// The opposite of DRY coding is, of course, WET. We Enjoy Typing.
