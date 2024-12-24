4b7aa55db8a5ae46def52b27a8d0b163f2f69d8f
Roderick <Roderickj800@gmail.com>
2024-12-24 06:26:44 -0500

    add numns

--- aa-projects/appAcademy/PostClass/javaScriptPractice.js
+++ aa-projects/appAcademy/PostClass/javaScriptPractice.js
@@ -6352,3 +6352,11 @@ debugger;
 console.log(shouldRecycle(waterBottle)); // 'Recycle Me!'
 
 // Adds up positive integers from 1-n
+
+function addNums(n) {
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+  }
+  return sum;
+}
