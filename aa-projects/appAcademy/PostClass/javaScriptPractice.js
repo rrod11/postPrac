28f4ba5d97cf8bac5b22463f430b50f917983680
Roderick <Roderickj800@gmail.com>
2025-01-12 08:28:31 -0500

    pseduocode

--- aa-projects/appAcademy/PostClass/javaScriptPractice.js
+++ aa-projects/appAcademy/PostClass/javaScriptPractice.js
@@ -6529,4 +6529,20 @@ function insertionSort(arr) {
 // In-place Insertion Sort
 // Mutates the original array
 
-function insertionSortInPlace(arr) {}
+function insertionSortInPlace(arr) {
+  /*
+  Pseudocode:
+
+  Set a pointer dividing the array into sorted and unsorted halves
+  Repeat while the unsorted half is not empty:
+  - make sure you have a console.log(sorted.join(',')) as your first line in the while loop
+  - Grab the first value from the unsorted half
+  - For each value starting from the divider,
+  - Check if the value to the left is smaller than the unsorted value
+  - If so, you've reached the insertion point so exit the loop
+  - If not shift the value to the right by 1 and continue
+  - Insert the unsorted value at the break point
+  - Increment the dividing pointer and repeat
+  Return the mutated array
+  */
+}
